eda8feda42386f0be14393cc980cc02f766dcb30
chenzaza <wxgych@gmail.com>
2022-02-05 15:49:34 +0800

    debug-windows failed check

--- Team31/Code31/src/spa/src/pkb/program_knowledge_base.cpp
+++ Team31/Code31/src/spa/src/pkb/program_knowledge_base.cpp
@@ -2,7 +2,7 @@
 using namespace spa;
 
 ProgramKnowledgeBase::ProgramKnowledgeBase(std::shared_ptr<Init> init)
-        : entities_ptr_(init) {
+        : entities_ptr_(init){
     proc_stmtlst_.resize(init->procedures.size());
     while_stmtlst_.resize(init->whiles.size());
     if_stmtlst_.resize(init->ifs.size() * 2);
