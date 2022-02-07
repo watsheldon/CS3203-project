e5e4ace4a73dc39ef85fb96f3e9172cc4243fdf4
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-02-08 07:02:03 +0800

    Implement recursive descent for validator

--- Team31/Code31/src/spa/src/source/validator.h
+++ Team31/Code31/src/spa/src/source/validator.h
@@ -16,7 +16,37 @@ class Validator {
     std::shared_ptr<std::vector<Token>> Validate();
 
   private:
+    static constexpr char kZero = '0';
+
     Tokenizer tokenizer_;
+    std::shared_ptr<std::vector<Token>> tokens_;
+    bool has_error_ = false;
+    std::string curr_token_;
+
+    void Program();
+    void Procedure();
+    void StmtLst();
+    void Stmt(bool first = false);
+    void Read();
+    void Print();
+    void Call();
+    void While();
+    void If();
+    void Assign();
+    void CondExpr();
+    void Expr();
+    void RelExpr();
+    void Term();
+    void Factor();
+    /**
+     * Returns true if curr_token_ is a valid constant
+     * Assumes that curr_token_ is not empty and if first char is a digit then
+     * the rest of the char are also digits.
+     */
+    bool IsConstant() const;
+    bool accept(SourceTokenType type);
+    bool expect(SourceTokenType type);
+    void fetchToken();
 };
 }
 
