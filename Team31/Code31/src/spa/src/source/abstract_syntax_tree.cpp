34fb090c9b860c99d891f6e12b460f76bc0f7da2
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-02-08 08:29:26 +0800

    AST: go through all tokens to perform counting

--- Team31/Code31/src/spa/src/source/abstract_syntax_tree.cpp
+++ Team31/Code31/src/spa/src/source/abstract_syntax_tree.cpp
@@ -1,10 +1,119 @@
 #include "abstract_syntax_tree.h"
+
+#include <utility>
 namespace spa {
 AbstractSyntaxTree::AbstractSyntaxTree(SharedVecToken tokens)
-        : tokens_(tokens) {
-
+        : tokens_(std::move(tokens)) {
+    int i = 0;
+    basic_entities_->procedures.emplace_back();
+    basic_entities_->variables.emplace_back();
+    basic_entities_->constants.emplace_back();
+    basic_entities_->reads.emplace_back();
+    basic_entities_->prints.emplace_back();
+    basic_entities_->calls.emplace_back();
+    basic_entities_->whiles.emplace_back();
+    basic_entities_->ifs.emplace_back();
+    basic_entities_->assigns.emplace_back();
+    int line_number = 1;
+    while (i < tokens_->size()) {
+        switch (tokens_->at(i).type) {
+            case kKeywordProcedure:AddProcedure(tokens_->at(i + 1).value);
+                i += 3;
+                break;
+            case kKeywordRead:basic_entities_->reads.emplace_back(line_number++);
+                AddVariable(tokens_->at(i + 1).value);
+                i += 3;
+                break;
+            case kKeywordPrint:basic_entities_->prints.emplace_back(line_number++);
+                AddVariable(tokens_->at(i + 1).value);
+                i += 3;
+                break;
+            case kKeywordCall:basic_entities_->calls.emplace_back(line_number++);
+                i += 3;
+                break;
+            case kKeywordWhile:basic_entities_->whiles.emplace_back(line_number++);
+                while (tokens->at(++i).type != kBraceL) {
+                    switch (tokens->at(i).type) {
+                        case kName:AddVariable(tokens->at(i).value);
+                            break;
+                        case kInteger:AddConstant(tokens->at(i).value);
+                            break;
+                        default:break;
+                    }
+                }
+                ++i;
+                break;
+            case kKeywordIf:basic_entities_->ifs.emplace_back(line_number++);
+                while (tokens->at(++i).type != kBraceL) {
+                    switch (tokens->at(i).type) {
+                        case kName:AddVariable(tokens->at(i).value);
+                            break;
+                        case kInteger:AddConstant(tokens->at(i).value);
+                            break;
+                        default:break;
+                    }
+                }
+                ++i;
+                break;
+            case kAssignEqual:AddVariable(tokens->at(i - 1).value);
+                basic_entities_->assigns.emplace_back(line_number++);
+                while (tokens->at(++i).type != kSemicolon) {
+                    switch (tokens->at(i).type) {
+                        case kName:AddVariable(tokens->at(i).value);
+                            break;
+                        case kInteger:AddConstant(tokens->at(i).value);
+                            break;
+                        default:break;
+                    }
+                }
+                ++i;
+                break;
+            case kKeywordThen:
+            case kKeywordElse:
+            case kBracketL:
+            case kBracketR:
+            case kBraceL:
+            case kBraceR:
+            case kOperatorPlus:
+            case kOperatorMinus:
+            case kOperatorTimes:
+            case kOperatorDivide:
+            case kOperatorModulo:
+            case kCondNot:
+            case kCondAnd:
+            case kCondOr:
+            case kRelLt:
+            case kRelLeq:
+            case kRelEq:
+            case kRelNeq:
+            case kRelGt:
+            case kRelGeq:
+            case kSemicolon:
+            case kName:
+            case kInteger:++i;
+                break;
+        }
+    }
 }
 std::shared_ptr<BasicEntities> AbstractSyntaxTree::getInitEntities() const {
-    return {};
+    return basic_entities_;
+}
+void AbstractSyntaxTree::AddProcedure(const std::string &name) {
+    auto[itr, b] = procedures_.emplace(name);
+    if (b) {
+        basic_entities_->procedures.emplace_back(name);
+    }
+}
+void AbstractSyntaxTree::AddVariable(const std::string &name) {
+    auto[itr, b] = variables_.emplace(name);
+    if (b) {
+        basic_entities_->variables.emplace_back(name);
+    }
+}
+void AbstractSyntaxTree::AddConstant(const std::string &value) {
+    auto[itr, b] = constants_.emplace(value);
+    if (b) {
+        basic_entities_->constants.emplace_back(value);
+    }
 }
 }
