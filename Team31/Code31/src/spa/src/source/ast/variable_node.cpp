ac44482154ae730013edff981d8ca7f4179a35be
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-02-13 02:16:14 +0800

    Finalize variable node

--- Team31/Code31/src/spa/src/source/ast/variable_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "variable_node.h"
-
-#include <memory>
-
-namespace spa {
-VariableNode::VariableNode(std::string name) : name_(std::move(name)) {}
-std::string VariableNode::getName() const { return name_; }
-}  // namespace spa
