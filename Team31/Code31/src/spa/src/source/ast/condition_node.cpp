204dac9ed1d13629329da3e65a1b8c83492d238d
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-02-13 01:25:50 +0800

    Finalize condition node

--- Team31/Code31/src/spa/src/source/ast/condition_node.cpp
+++ Team31/Code31/src/spa/src/source/ast/condition_node.cpp
@@ -1,22 +1,20 @@
 #include "condition_node.h"
 
-#include <utility>
-
 #include "constant_node.h"
 #include "variable_node.h"
 
 namespace spa {
 
-void ConditionNode::AddVariable(std::shared_ptr<VariableNode> variable) {
-    variables_.emplace(std::move(variable));
+void ConditionNode::AddVariable(const VariableNode *variable) {
+    variables_.emplace(variable);
 }
-void ConditionNode::AddConstant(std::shared_ptr<ConstantNode> constant) {
-    constants_.emplace(std::move(constant));
+void ConditionNode::AddConstant(const ConstantNode *constant) {
+    constants_.emplace(constant);
 }
-ConditionNode::SharedPtrSet<VariableNode> ConditionNode::get_variables() const {
+const std::set<const VariableNode *> &ConditionNode::get_variables() const {
     return variables_;
 }
-ConditionNode::SharedPtrSet<ConstantNode> ConditionNode::get_constants() const {
+const std::set<const ConstantNode *> &ConditionNode::get_constants() const {
     return constants_;
 }
 }  // namespace spa
