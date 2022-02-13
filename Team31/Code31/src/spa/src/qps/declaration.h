93439c81b64909a530970343fe926030db011d8c
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-02-13 19:25:34 +0800

    Modify Declaration.h

--- Team31/Code31/src/spa/src/qps/declaration.h
+++ Team31/Code31/src/spa/src/qps/declaration.h
@@ -1,28 +1,59 @@
 #ifndef SRC_SPA_SRC_QUERYPROCESSINGSUBSYSTEM_DECLARATION_H_
 #define SRC_SPA_SRC_QUERYPROCESSINGSUBSYSTEM_DECLARATION_H_
 #include<string>
+#include <utility>
 
 namespace spa {
-    enum DeclarationType{
-        STMT,
-        READ,
-        PRINT,
-        CALL,
-        WHILE,
-        IF,
-        ASSIGN,
-        VARIABLE,
-        CONSTANT,
-        PROCEDURE,
-    };
+enum class DeclarationType{
+    STMT,
+    READ,
+    PRINT,
+    CALL,
+    WHILE,
+    IF,
+    ASSIGN,
+    VARIABLE,
+    CONSTANT,
+    PROCEDURE,
+};
 
-    class Declaration{
-        DeclarationType type;
-        std::string synonym;
-      public:
-        Declaration(DeclarationType type, std::string synonym);
-        DeclarationType getDeclarationType();
-        std::string getDeclarationSynonym();
-    };
+std::string_view keyword(DeclarationType type) {
+    switch(type) {
+        case DeclarationType::STMT:
+            return "stmt";
+        case DeclarationType::READ:
+            return "read";
+        case DeclarationType::PRINT:
+            return "print";
+        case DeclarationType::CALL:
+            return "call";
+        case DeclarationType::WHILE:
+            return "while";
+        case DeclarationType::IF:
+            return "if";
+        case DeclarationType::ASSIGN:
+            return "assign";
+        case DeclarationType::VARIABLE:
+            return "variable";
+        case DeclarationType::CONSTANT:
+            return "constant";
+        case DeclarationType::PROCEDURE:
+            return "procedure";
+    }
 }
-#endif //SPA_TEAM31_CODE31_SRC_SPA_SRC_QUERYPROCESSINGSUBSYSTEM_DECLARATION_H_
+
+class Declaration{
+    DeclarationType type;
+    std::string synonym;
+  public:
+    Declaration(DeclarationType type, std::string synonym)
+            :type(type), synonym(std::move(synonym)) {}
+    DeclarationType getDeclarationType() {
+        return this->type;
+    }
+    std::string getDeclarationSynonym() {
+        return this->synonym;
+    }
+};
+}
+#endif //SRC_SPA_SRC_QUERYPROCESSINGSUBSYSTEM_DECLARATION_H_
