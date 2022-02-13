90426d82ac39f28b550049621c763344eaa31661
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-02-13 17:48:53 +0800

    Update Select and Add Pattern and SuchThat class

--- Team31/Code31/src/spa/src/qps/select.h
+++ Team31/Code31/src/spa/src/qps/select.h
@@ -9,9 +9,15 @@ class Select{
     DeclarationType type;
     std::string synonym;
   public:
-    Select(DeclarationType d_type, std::string s_synonym);
-    DeclarationType getSelectType();
-    std::string getSelectSynonym();
+    Select(DeclarationType d_type, std::string s_synonym)
+            : type(d_type), synonym(s_synonym) {}
+
+    DeclarationType getSelectType() {
+        return this->type;
+    }
+    std::string getSelectSynonym() {
+        return this->synonym;
+    }
 };
 }
 #endif //INC_21S2_CP_SPA_TEAM_31_TEAM31_CODE31_SRC_SPA_SRC_QPS_SELECT_H_
--- Team31/Code31/src/spa/src/qps/such_that.h
+++ Team31/Code31/src/spa/src/qps/such_that.h
@@ -1,16 +1,39 @@
 #ifndef SRC_SPA_SRC_QUERYPROCESSINGSUBSYSTEM_SUCHTHAT_H_
 #define SRC_SPA_SRC_QUERYPROCESSINGSUBSYSTEM_SUCHTHAT_H_
 
+#include <string>
+
 namespace spa {
-    enum class SuchThat{
-        FOLLOWS,
-        FOLLOWS_T,
-        PARENT,
-        PARENT_T,
-        USES_S,
-        USES_P,
-        MODIFIES_S,
-        MODIFIES_P,
-    };
+enum class SuchThatType{
+    FOLLOWS,
+    FOLLOWS_T,
+    PARENT,
+    PARENT_T,
+    USES_S,
+    USES_P,
+    MODIFIES_S,
+    MODIFIES_P,
+};
+class SuchThat{
+  private:
+    SuchThatType type;
+    std::string ref1;
+    std::string ref2;
+  public :
+    SuchThat(SuchThatType type, std::string ref1, std::string ref2)
+        : type(type), ref1(ref1), ref2(ref2) {}
+
+    SuchThatType getType() {
+        return this->type;
+    }
+
+    std::string getRef1() {
+        return this->ref1;
+    }
+
+    std::string getRef2() {
+        return this->ref2;
+    }
+};
 }
 #endif //SPA_TEAM31_CODE31_SRC_SPA_SRC_QUERYPROCESSINGSUBSYSTEM_SUCHTHAT_H_
