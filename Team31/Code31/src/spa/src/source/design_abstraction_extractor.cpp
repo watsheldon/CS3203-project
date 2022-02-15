04171133226f770b29ec499c176783ef851e8d47
watsheldon <chinaempire.cjw@gmail.com>
2022-02-16 00:58:06 +0800

    reduce variable scope

--- Team31/Code31/src/spa/src/source/design_abstraction_extractor.cpp
+++ Team31/Code31/src/spa/src/source/design_abstraction_extractor.cpp
@@ -30,8 +30,8 @@ void DesignAbstractionExtractor::Visit(const ProgramNode &program_node) {
 
 void DesignAbstractionExtractor::Visit(const ProcedureNode &procedure_node) {
     // Links procedure with its statement list via their indices
-    auto stmt_lst = procedure_node.GetStmtlst();
     int procedure_index = procedure_node.GetIndex();
+    auto stmt_lst = procedure_node.GetStmtlst();
     int stmt_lst_index = stmt_lst->GetIndex();
     pkb_->SetIndex(Index<SetEntityType::kProc>(procedure_index),
                    Index<SetEntityType::kStmtLst>(stmt_lst_index));
@@ -40,12 +40,12 @@ void DesignAbstractionExtractor::Visit(const ProcedureNode &procedure_node) {
 }
 
 void DesignAbstractionExtractor::Visit(const StmtLstNode &stmt_lst_node) {
-    int stmt_lst_index = stmt_lst_node.GetIndex();
     std::vector<int> statement_indices;
     for (auto statement : stmt_lst_node.GetStatements()) {
         statement_indices.emplace_back(statement->GetIndex());
         statement->Accept(*this);
     }
+    int stmt_lst_index = stmt_lst_node.GetIndex();
 
     // Links each StmtLst to the indices of the containing statements.
     pkb_->SetLst(Index<SetEntityType::kStmtLst>(stmt_lst_index),
@@ -77,7 +77,6 @@ void DesignAbstractionExtractor::Visit(const CallNode &call_node) {
 }
 
 void DesignAbstractionExtractor::Visit(const IfNode &if_node) {
-    int stmt_no = if_node.GetIndex();
     IfNode::IfStmtLst if_stmt_lst = if_node.GetStmtLsts();
     auto then_lst = if_stmt_lst.then_lst;
     auto else_lst = if_stmt_lst.else_lst;
@@ -85,6 +84,7 @@ void DesignAbstractionExtractor::Visit(const IfNode &if_node) {
     // Links if_statement to then_stmtLst and else_stmtLst by their indices
     int then_lst_index = then_lst->GetIndex();
     int else_lst_index = else_lst->GetIndex();
+    int stmt_no = if_node.GetIndex();
     pkb_->SetIndex(Index<SetEntityType::kStmt>(stmt_no),
                    Index<SetEntityType::kStmtLst>(then_lst_index),
                    Index<SetEntityType::kStmtLst>(else_lst_index));
@@ -97,11 +97,10 @@ void DesignAbstractionExtractor::Visit(const IfNode &if_node) {
 }
 
 void DesignAbstractionExtractor::Visit(const WhileNode &while_node) {
-    int stmt_no = while_node.GetIndex();
-
     // Links while statement to its statement list by their indices.
     auto stmt_lst = while_node.GetStmtlst();
     int stmt_lst_index = stmt_lst->GetIndex();
+    int stmt_no = while_node.GetIndex();
     pkb_->SetIndex(Index<SetEntityType::kStmt>(stmt_no),
                    Index<SetEntityType::kStmtLst>(stmt_lst_index));
 
@@ -112,22 +111,20 @@ void DesignAbstractionExtractor::Visit(const WhileNode &while_node) {
 }
 
 void DesignAbstractionExtractor::Visit(const ReadNode &read_node) {
-    int stmt_no = read_node.GetIndex();
-
     // Extracts direct Modifies relationship from read statement.
     auto modified_var = read_node.GetVariable();
     int var_index = modified_var->GetIndex();
+    int stmt_no = read_node.GetIndex();
     pkb_->SetRel(Index<SetEntityType::kStmt>(stmt_no),
                  Index<SetEntityType::kVar>(var_index));
 }
 
 void DesignAbstractionExtractor::Visit(const PrintNode &print_node) {
-    int stmt_no = print_node.GetIndex();
-
     // Extracts direct Uses relationship from print statement.
     auto used_var = print_node.GetVariable();
     std::vector<int> used_var_index;
     used_var_index.emplace_back(used_var->GetIndex());
+    int stmt_no = print_node.GetIndex();
     pkb_->SetRel(Index<SetEntityType::kStmt>(stmt_no), used_var_index);
 }
 
