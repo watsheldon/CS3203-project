35d226b65a91f8126d4ea2549e0bc6c76938afc9
chenzaza <wxgych@gmail.com>
2022-02-20 21:09:32 +0800

    Unit test for constant_value_store

--- Team31/Code31/src/unit_testing/src/pkb/store/constant_value_store_test.cpp
+++ Team31/Code31/src/unit_testing/src/pkb/store/constant_value_store_test.cpp
@@ -1,1 +1,15 @@
+#include "catch.hpp"
 
+#include "pkb/store/constant_value_store.h"
+
+using namespace spa;
+
+TEST_CASE("pkb/store/ConstantValueStore") {
+    ConstantValueStore cvs({std::string(), "1", "2", "3"});
+    SECTION("GetValue") { REQUIRE(cvs.GetValue(3) == "3"); }
+    SECTION("GetIndex") { REQUIRE(cvs.GetIndex("3") == 3); }
+    SECTION("GetInvalidIndex") { REQUIRE(cvs.GetIndex("6") == 0); }
+    std::vector<std::string> values = {std::string(), "1", "2", "3"};
+    SECTION("GetAllValues") { REQUIRE(cvs.GetAllValues() == values); }
+    SECTION("size") { REQUIRE(cvs.size() == 3); }
+}
