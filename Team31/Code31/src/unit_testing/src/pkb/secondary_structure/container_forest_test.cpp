26950b7367fbdfe281e1c640ffea10b7cd0c37ad
chenzaza <wxgych@gmail.com>
2022-02-21 22:46:18 +0800

    Add unit tests for container

---
+++ Team31/Code31/src/unit_testing/src/pkb/secondary_structure/container_forest_test.cpp
@@ -0,0 +1,39 @@
+#include "pkb/secondary_structure/container_forest.h"
+
+#include "catch.hpp"
+#include "pkb/store/stmtlst_parent_store.h"
+#include "pkb/store/stmtlst_statements_store.h"
+
+namespace spa {
+TEST_CASE("pkb/secondary_structure/ContainerForest") {
+    StmtlstStatementsStore sss(5, 10);
+    sss.Set(1, std::vector<int>{1, 10});
+    sss.Set(2, std::vector<int>{2, 3, 4});
+    sss.Set(3, std::vector<int>{5, 8});
+    sss.Set(4, std::vector<int>{6, 7});
+    sss.Set(5, std::vector<int>{9});
+    StmtlstParentStore sps(1, 10, 5);
+    sps.Set(Index<SetEntityType::kProc>(1), Index<SetEntityType::kStmtLst>(1));
+    sps.Set(Index<SetEntityType::kStmt>(1), Index<SetEntityType::kStmtLst>(2));
+    sps.Set(Index<SetEntityType::kStmt>(5), Index<SetEntityType::kStmtLst>(4));
+    sps.Set(Index<SetEntityType::kStmt>(4), Index<SetEntityType::kStmtLst>(3),
+            Index<SetEntityType::kStmtLst>(5));
+    ContainerForest cf(sps, sss, 5);
+    SECTION("IsParentT") {
+        REQUIRE(cf.IsParentT(3, 4));
+        REQUIRE_FALSE(cf.IsParentT(3, 5));
+    }
+    SECTION("GetParents") {
+        REQUIRE(cf.GetParents(1).empty());
+        REQUIRE(cf.GetParents(5) == std::vector<int>{2, 1});
+    }
+    SECTION("GetChildren") {
+        REQUIRE(cf.GetChildren(5).empty());
+        REQUIRE(cf.GetChildren(3) == std::vector<int>{4});
+    }
+    SECTION("GetRightmostGrandchild") {
+        REQUIRE(cf.GetRightmostGrandchild(1) == 5);
+    }
+}
+
+}  // namespace spa
