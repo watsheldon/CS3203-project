1daa8ae477797c845e75657b68bc4d6f08a93b48
watsheldon <chinaempire.cjw@gmail.com>
2022-02-22 16:19:31 +0800

    create placeholder for ast unit test

---
+++ Team31/Code31/src/unit_testing/src/source/ast/abstract_syntax_tree_test.cpp
@@ -0,0 +1,11 @@
+#include "source/ast/abstract_syntax_tree.h"
+
+#include "catch.hpp"
+
+namespace spa {
+TEST_CASE("source/ast/AbstractSyntaxTree") {
+    SECTION("AbstractSyntaxTree constructor") {}
+    SECTION("GetInitEntities") {}
+    SECTION("GetRoot") {}
+}
+}  // namespace spa
