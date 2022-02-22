2bf2b82f319c5c2a96c05a76c24678493dba7fd5
watsheldon <chinaempire.cjw@gmail.com>
2022-02-22 23:06:54 +0800

    add tokenizer unit tests after bug fix

--- Team31/Code31/src/unit_testing/src/common/tokenizer_test.cpp
+++ Team31/Code31/src/unit_testing/src/common/tokenizer_test.cpp
@@ -28,11 +28,35 @@ TEST_CASE("common/Tokenizer") {
         REQUIRE(tokenizer3.Next() == "");
     }
 
+    SECTION("next (while statement with negate condition)") {
+        std::string_view code6{"procedure while{while(!(while!=0)){print while;}}"};
+        Tokenizer tokenizer6(code6);
+        REQUIRE(tokenizer6.Next() == "procedure");
+        REQUIRE(tokenizer6.Next() == "while");
+        REQUIRE(tokenizer6.Next() == "{");
+        REQUIRE(tokenizer6.Next() == "while");
+        REQUIRE(tokenizer6.Next() == "(");
+        REQUIRE(tokenizer6.Next() == "!");
+        REQUIRE(tokenizer6.Next() == "(");
+        REQUIRE(tokenizer6.Next() == "while");
+        REQUIRE(tokenizer6.Next() == "!=");
+        REQUIRE(tokenizer6.Next() == "0");
+        REQUIRE(tokenizer6.Next() == ")");
+        REQUIRE(tokenizer6.Next() == ")");
+        REQUIRE(tokenizer6.Next() == "{");
+        REQUIRE(tokenizer6.Next() == "print");
+        REQUIRE(tokenizer6.Next() == "while");
+        REQUIRE(tokenizer6.Next() == ";");
+        REQUIRE(tokenizer6.Next() == "}");
+        REQUIRE(tokenizer6.Next() == "}");
+        REQUIRE(tokenizer6.Next() == "");
+    }
+
     SECTION("next (comprehensive tests with whitespaces)") {
         std::string_view code4{
                 "procedure sumDigits {\n\tread number;\n\tsum = 0;\nwhile ( ! "
-                "\n (\n number > 0 \t) ) { digit = number % 10; while (a == 0 "
-                "&& b <= 0) { while (a != b || b >= c) { a = b;\n }\n } sum = "
+                "\n (\n\n\n number > 0 \t) ) \n\n{ digit = number % 10; while (a == 0 "
+                "&& b <= 0) { while (a != b || b >= c) { a = b;\n\n }\n } sum = "
                 "sum + digit; number = number / 10; }call Haha233test1; print "
                 "sum;}procedure Haha233test1 {    if (CS3203-A < 6*100 ) then "
                 "{    	read read;    } else {print Haha233test1;}}"};
@@ -141,7 +165,7 @@ TEST_CASE("common/Tokenizer") {
         REQUIRE(tokenizer4.Next() == "");
     }
 
-    SECTION("next (comprehensive tests with whitespaces stripped") {
+    SECTION("next (comprehensive tests with whitespaces stripped)") {
         std::string_view code5{
                 "procedure sumDigits{read "
                 "number;sum=0;while(!(number>0)){digit=number%10;while(a==0&&b<"
@@ -150,6 +174,108 @@ TEST_CASE("common/Tokenizer") {
                 "Haha233test1{if(CS3203-A<6*100)then{read read;}else{print "
                 "Haha233test1;}}"};
         Tokenizer tokenizer5(code5);
+
+        REQUIRE(tokenizer5.Next() == "procedure");
+        REQUIRE(tokenizer5.Next() == "sumDigits");
+        REQUIRE(tokenizer5.Next() == "{");
+        REQUIRE(tokenizer5.Next() == "read");
+        REQUIRE(tokenizer5.Next() == "number");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "sum");
+        REQUIRE(tokenizer5.Next() == "=");
+        REQUIRE(tokenizer5.Next() == "0");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "while");
+        REQUIRE(tokenizer5.Next() == "(");
+        REQUIRE(tokenizer5.Next() == "!");
+        REQUIRE(tokenizer5.Next() == "(");
+        REQUIRE(tokenizer5.Next() == "number");
+        REQUIRE(tokenizer5.Next() == ">");
+        REQUIRE(tokenizer5.Next() == "0");
+        REQUIRE(tokenizer5.Next() == ")");
+        REQUIRE(tokenizer5.Next() == ")");
+        REQUIRE(tokenizer5.Next() == "{");
+        REQUIRE(tokenizer5.Next() == "digit");
+        REQUIRE(tokenizer5.Next() == "=");
+        REQUIRE(tokenizer5.Next() == "number");
+        REQUIRE(tokenizer5.Next() == "%");
+        REQUIRE(tokenizer5.Next() == "10");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "while");
+        REQUIRE(tokenizer5.Next() == "(");
+        REQUIRE(tokenizer5.Next() == "a");
+        REQUIRE(tokenizer5.Next() == "==");
+        REQUIRE(tokenizer5.Next() == "0");
+        REQUIRE(tokenizer5.Next() == "&&");
+        REQUIRE(tokenizer5.Next() == "b");
+        REQUIRE(tokenizer5.Next() == "<=");
+        REQUIRE(tokenizer5.Next() == "0");
+        REQUIRE(tokenizer5.Next() == ")");
+        REQUIRE(tokenizer5.Next() == "{");
+        REQUIRE(tokenizer5.Next() == "while");
+        REQUIRE(tokenizer5.Next() == "(");
+        REQUIRE(tokenizer5.Next() == "a");
+        REQUIRE(tokenizer5.Next() == "!=");
+        REQUIRE(tokenizer5.Next() == "b");
+        REQUIRE(tokenizer5.Next() == "||");
+        REQUIRE(tokenizer5.Next() == "b");
+        REQUIRE(tokenizer5.Next() == ">=");
+        REQUIRE(tokenizer5.Next() == "c");
+        REQUIRE(tokenizer5.Next() == ")");
+        REQUIRE(tokenizer5.Next() == "{");
+        REQUIRE(tokenizer5.Next() == "a");
+        REQUIRE(tokenizer5.Next() == "=");
+        REQUIRE(tokenizer5.Next() == "b");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "}");
+        REQUIRE(tokenizer5.Next() == "}");
+        REQUIRE(tokenizer5.Next() == "sum");
+        REQUIRE(tokenizer5.Next() == "=");
+        REQUIRE(tokenizer5.Next() == "sum");
+        REQUIRE(tokenizer5.Next() == "+");
+        REQUIRE(tokenizer5.Next() == "digit");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "number");
+        REQUIRE(tokenizer5.Next() == "=");
+        REQUIRE(tokenizer5.Next() == "number");
+        REQUIRE(tokenizer5.Next() == "/");
+        REQUIRE(tokenizer5.Next() == "10");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "}");
+        REQUIRE(tokenizer5.Next() == "call");
+        REQUIRE(tokenizer5.Next() == "Haha233test1");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "print");
+        REQUIRE(tokenizer5.Next() == "sum");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "}");
+        REQUIRE(tokenizer5.Next() == "procedure");
+        REQUIRE(tokenizer5.Next() == "Haha233test1");
+        REQUIRE(tokenizer5.Next() == "{");
+        REQUIRE(tokenizer5.Next() == "if");
+        REQUIRE(tokenizer5.Next() == "(");
+        REQUIRE(tokenizer5.Next() == "CS3203");
+        REQUIRE(tokenizer5.Next() == "-");
+        REQUIRE(tokenizer5.Next() == "A");
+        REQUIRE(tokenizer5.Next() == "<");
+        REQUIRE(tokenizer5.Next() == "6");
+        REQUIRE(tokenizer5.Next() == "*");
+        REQUIRE(tokenizer5.Next() == "100");
+        REQUIRE(tokenizer5.Next() == ")");
+        REQUIRE(tokenizer5.Next() == "then");
+        REQUIRE(tokenizer5.Next() == "{");
+        REQUIRE(tokenizer5.Next() == "read");
+        REQUIRE(tokenizer5.Next() == "read");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "}");
+        REQUIRE(tokenizer5.Next() == "else");
+        REQUIRE(tokenizer5.Next() == "{");
+        REQUIRE(tokenizer5.Next() == "print");
+        REQUIRE(tokenizer5.Next() == "Haha233test1");
+        REQUIRE(tokenizer5.Next() == ";");
+        REQUIRE(tokenizer5.Next() == "}");
+        REQUIRE(tokenizer5.Next() == "}");
+        REQUIRE(tokenizer5.Next() == "");
     }
 }
 }  // namespace spa
