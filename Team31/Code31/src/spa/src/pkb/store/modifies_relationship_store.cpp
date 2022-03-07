6e2c4a2cd77fdf3a36070e694300e3878a6ce26d
watsheldon <chinaempire.cjw@gmail.com>
2022-03-08 03:29:49 +0800

    refactor use and modify stores

--- Team31/Code31/src/spa/src/pkb/store/modifies_relationship_store.cpp
+++ Team31/Code31/src/spa/src/pkb/store/modifies_relationship_store.cpp
@@ -3,19 +3,18 @@
 namespace spa {
 spa::ModifiesRelationshipStore::ModifiesRelationshipStore(size_t stmtSize,
                                                           size_t varSize)
-        : stmt_to_var_(stmtSize + 1, 0), var_to_stmtlst_(varSize + 1) {}
+        : stmt_var_(stmtSize, varSize) {}
 
 void spa::ModifiesRelationshipStore::Set(int stmt_no, int var_index) {
-    stmt_to_var_[stmt_no] = var_index;
-    var_to_stmtlst_[var_index].emplace_back(stmt_no);
+    stmt_var_.Set(stmt_no, std::vector<int>{var_index});
 }
 
 const std::vector<int>& spa::ModifiesRelationshipStore::GetStmtNo(
         int var_index) const {
-    return var_to_stmtlst_[var_index];
+    return stmt_var_.GetKeys(var_index);
 }
 
 int spa::ModifiesRelationshipStore::GetVarIndex(int stmt_no) const {
-    return stmt_to_var_[stmt_no];
+    return stmt_var_.GetVals(stmt_no)[0];
 }
 }  // namespace spa
--- Team31/Code31/src/spa/src/pkb/store/uses_relationship_store.cpp
+++ Team31/Code31/src/spa/src/pkb/store/uses_relationship_store.cpp
@@ -2,18 +2,17 @@
 
 namespace spa {
 UsesRelationshipStore::UsesRelationshipStore(size_t stmtSize, size_t varSize)
-        : IndexBimap<std::vector<int>>(stmtSize, varSize) {}
+        : stmt_var_(stmtSize, varSize) {}
 
 void UsesRelationshipStore::Set(int stmt_no, std::vector<int>&& var_indices) {
-    IndexBimap<std::vector<int>>::Set(
-            stmt_no, std::forward<std::vector<int>>(var_indices));
+    stmt_var_.Set(stmt_no, std::forward<std::vector<int>>(var_indices));
 }
 
 const std::vector<int>& UsesRelationshipStore::GetVarIndex(int stmt_no) const {
-    return IndexBimap<std::vector<int>>::GetVals(stmt_no);
+    return stmt_var_.GetVals(stmt_no);
 }
 
 const std::vector<int>& UsesRelationshipStore::GetStmtNo(int var_index) const {
-    return IndexBimap<std::vector<int>>::GetKeys(var_index);
+    return stmt_var_.GetKeys(var_index);
 }
 }  // namespace spa
