f12aa94f0958aa5b9dcb6c839b927bb665b43d98
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-03-13 20:50:39 +0800

    QPS: code quality improvements

--- Team31/Code31/src/spa/src/qps/PQL_validator.cpp
+++ Team31/Code31/src/spa/src/qps/PQL_validator.cpp
@@ -1,12 +1,12 @@
 #include "PQL_validator.h"
 
+#include <algorithm>
 #include <string_view>
 #include <vector>
 
 #include "query_token.h"
 
 namespace spa {
-
 std::vector<QueryToken> PQLValidator::Validate(std::string_view value) {
     tokens_.clear();
     tokenizer_(value);
@@ -14,16 +14,12 @@ std::vector<QueryToken> PQLValidator::Validate(std::string_view value) {
     bool valid_query = Query();
     return valid_query ? std::move(tokens_) : std::vector<QueryToken>();
 }
-
 bool PQLValidator::Query() { return Declaration() && Select(); }
-
 bool PQLValidator::IsConstant() {
     if (!std::isdigit(curr_token_[0])) return false;
     return curr_token_.length() == 1 || curr_token_[0] != kZero;
 }
-
 void PQLValidator::FetchToken() { tokenizer_ >> curr_token_; }
-
 bool PQLValidator::Accept(QueryTokenType type) {
     if (curr_token_.empty()) return false;
     if (type < QueryTokenType::kWord) {
@@ -46,20 +42,17 @@ bool PQLValidator::Accept(QueryTokenType type) {
     }
     return false;
 }
-
 bool PQLValidator::Declaration() {
     while (SynDecl()) {
         if (!MultipleSynonym()) return false;
     }
     return true;
 }
-
 bool PQLValidator::SynDecl() {
     return std::any_of(
             kDeclTypes.begin(), kDeclTypes.end(),
             [this](const QueryTokenType type) { return Accept(type); });
 }
-
 bool PQLValidator::MultipleSynonym() {
     if (!Accept(QueryTokenType::kWord)) return false;
     while (Accept(QueryTokenType::kComma)) {
@@ -69,7 +62,6 @@ bool PQLValidator::MultipleSynonym() {
     }
     return Accept(QueryTokenType::kSemicolon);
 }
-
 bool PQLValidator::Select() {
     if (!Accept(QueryTokenType::kKeywordSelect)) return false;
     if (!Accept(QueryTokenType::kWord)) return false;
@@ -85,58 +77,44 @@ bool PQLValidator::Select() {
     }
     return valid && curr_token_.empty();
 }
-
 bool PQLValidator::SuchThat() {
     if (!Accept(QueryTokenType::kKeywordThat)) return false;
-    if (Accept(QueryTokenType::kKeywordFollows)) {
+    if (Accept(QueryTokenType::kKeywordFollows) ||
+        Accept(QueryTokenType::kKeywordParent)) {
         Accept(QueryTokenType::kOperatorTimes);
         return FollowsParent();
     }
-    if (Accept(QueryTokenType::kKeywordParent)) {
-        Accept(QueryTokenType::kOperatorTimes);
-        return FollowsParent();
-    }
-    if (Accept(QueryTokenType::kKeywordUses)) {
-        return UsesModifies();
-    }
-    if (Accept(QueryTokenType::kKeywordModifies)) {
+    if (Accept(QueryTokenType::kKeywordUses) ||
+        Accept(QueryTokenType::kKeywordModifies)) {
         return UsesModifies();
     }
     return false;
 }
-
 bool PQLValidator::FollowsParent() {
     return Accept(QueryTokenType::kBracketL) && StmtRef() &&
            Accept(QueryTokenType::kComma) && StmtRef() &&
            Accept(QueryTokenType::kBracketR);
 }
-
 bool PQLValidator::UsesModifies() {
     return Accept(QueryTokenType::kBracketL) && UsesModifiesStmtEntRef() &&
            Accept(QueryTokenType::kComma) && EntRef() &&
            Accept(QueryTokenType::kBracketR);
 }
-
 // The first argument for Modifies and Uses cannot be UNDERSCORE
 // as it is unclear whether _ refers to a statement or procedure
 bool PQLValidator::UsesModifiesStmtEntRef() {
     return Accept(QueryTokenType::kWord) || Accept(QueryTokenType::kInteger);
 }
 bool PQLValidator::Pattern() {
-    if (!Accept(QueryTokenType::kWord)) return false;
-    if (!Accept(QueryTokenType::kBracketL)) return false;
-    if (!EntRef()) return false;
-    if (!Accept(QueryTokenType::kComma)) return false;
-    ExpressionSpec();
-    if (!Accept(QueryTokenType::kBracketR)) return false;
-    return true;
+    return Accept(QueryTokenType::kWord) && Accept(QueryTokenType::kBracketL) &&
+           EntRef() && Accept(QueryTokenType::kComma) && ExpressionSpec() &&
+           Accept(QueryTokenType::kBracketR);
 }
 bool PQLValidator::StmtRef() {
     return Accept(QueryTokenType::kWord) ||
            Accept(QueryTokenType::kUnderscore) ||
            Accept(QueryTokenType::kInteger);
 }
-
 bool PQLValidator::EntRef() {
     return Accept(QueryTokenType::kWord) ||
            Accept(QueryTokenType::kUnderscore) || Identifier();
@@ -157,12 +135,10 @@ bool PQLValidator::ExpressionSpec() {
     return ArithmeticExpr() && Accept(QueryTokenType::kQuote) &&
            (not_wild || Accept(QueryTokenType::kUnderscore));
 }
-
 bool PQLValidator::ArithOpr() {
     return std::any_of(kArithmeticOpr.begin(), kArithmeticOpr.end(),
                        [this](QueryTokenType type) { return Accept(type); });
 }
-
 bool PQLValidator::ArithmeticExpr(bool has_left /*= false */) {
     if (Accept(QueryTokenType::kBracketL)) {
         if (!Group()) return false;
@@ -178,7 +154,6 @@ bool PQLValidator::ArithmeticExpr(bool has_left /*= false */) {
 bool PQLValidator::Group() {
     return ArithmeticExpr() && Accept(QueryTokenType::kBracketR);
 }
-
 bool PQLValidator::Factor() {
     return Accept(QueryTokenType::kInteger) || Accept(QueryTokenType::kWord);
 }
@@ -186,5 +161,4 @@ bool PQLValidator::With() {
     // todo : new condition clause
     return false;
 }
-
 }  // namespace spa
