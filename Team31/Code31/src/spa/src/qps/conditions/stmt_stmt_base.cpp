a3504e5166154c985037933e38ea04704c3c06d8
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-03-21 01:40:00 +0800

    Avoid redundant move operations

--- Team31/Code31/src/spa/src/qps/conditions/stmt_stmt_base.cpp
+++ Team31/Code31/src/spa/src/qps/conditions/stmt_stmt_base.cpp
@@ -8,17 +8,11 @@ StmtStmtBase::StmtStmtBase() : type_(Type::kWildWild) {}
 StmtStmtBase::StmtStmtBase(int first, int second)
         : type_(Type::kIntInt), first_int_(first), second_int_(second) {}
 StmtStmtBase::StmtStmtBase(int first, Synonym *second)
-        : type_(Type::kIntSyn),
-          first_int_(first),
-          second_syn_(std::move(second)) {}
+        : type_(Type::kIntSyn), first_int_(first), second_syn_(second) {}
 StmtStmtBase::StmtStmtBase(Synonym *first, int second)
-        : type_(Type::kSynInt),
-          first_syn_(std::move(first)),
-          second_int_(second) {}
+        : type_(Type::kSynInt), first_syn_(first), second_int_(second) {}
 StmtStmtBase::StmtStmtBase(Synonym *first, Synonym *second)
-        : type_(Type::kSynSyn),
-          first_syn_(std::move(first)),
-          second_syn_(std::move(second)) {}
+        : type_(Type::kSynSyn), first_syn_(first), second_syn_(second) {}
 StmtStmtBase::StmtStmtBase(ArgPos pos, int integer) {
     if (pos == ArgPos::kFirst) {
         type_ = Type::kIntWild;
