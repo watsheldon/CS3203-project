a431393594bc3805cb79417722f0f4ab5dbd4941
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-03-21 22:02:09 +0800

    Add CallsClause and CallsTransClause

---
+++ Team31/Code31/src/spa/src/qps/conditions/calls_trans_clause.cpp
@@ -0,0 +1,62 @@
+#include "calls_trans_clause.h"
+
+#include <cassert>
+#include <utility>
+
+#include "common/entity_type_enum.h"
+#include "common/index.h"
+#include "common/type_convert_helpers.h"
+#include "pkb/knowledge_base.h"
+#include "qps/evaluator/result_table.h"
+
+namespace spa {
+ResultTable CallsTransClause::Execute(
+        KnowledgeBase *knowledge_base) const noexcept {
+    switch (type_) {
+        case kSynSyn: {
+            auto [col_1, col_2] = knowledge_base->GetCallsTPairs();
+            return {first_syn_, std::move(col_1), second_syn_,
+                    std::move(col_2)};
+        }
+        case kSynWild: {
+            auto result = knowledge_base->GetCalls(ArgPos::kFirst);
+            return {first_syn_, std::move(result)};
+        }
+        case kSynIdent: {
+            auto result = knowledge_base->GetCallsT(
+                    Name<ArgPos::kSecond>(second_ident_));
+            return {first_syn_, std::move(result)};
+        }
+        case kWildSyn: {
+            auto result = knowledge_base->GetCalls(ArgPos::kSecond);
+            return {second_syn_, std::move(result)};
+        }
+        case kWildWild: {
+            auto result = knowledge_base->ExistCalls();
+            return ResultTable(result);
+        }
+        case kWildIdent: {
+            auto result = knowledge_base->ExistCalls(
+                    Name<ArgPos::kSecond>(second_ident_));
+            return ResultTable(result);
+        }
+        case kIdentSyn: {
+            auto result = knowledge_base->GetCallsT(
+                    Name<ArgPos::kFirst>(first_ident_));
+            return {second_syn_, std::move(result)};
+        }
+        case kIdentWild: {
+            auto result = knowledge_base->ExistCalls(
+                    Name<ArgPos::kFirst>(first_ident_));
+            return ResultTable(result);
+        }
+        case kIdentIdent: {
+            auto result =
+                    knowledge_base->ExistCallsT(first_ident_, second_ident_);
+            return ResultTable(result);
+        }
+        default:
+            assert(false);
+    }
+}
+}  // namespace spa
---
+++ Team31/Code31/src/spa/src/qps/conditions/calls_trans_clause.h
@@ -0,0 +1,15 @@
+#ifndef SRC_SPA_SRC_QPS_CONDITIONS_CALLS_TRANS_CLAUSE_H_
+#define SRC_SPA_SRC_QPS_CONDITIONS_CALLS_TRANS_CLAUSE_H_
+
+#include "calls_base.h"
+#include "pkb/knowledge_base.h"
+#include "qps/evaluator/result_table.h"
+
+namespace spa {
+class CallsTransClause : public CallsBase {
+    using CallsBase::CallsBase;
+    ResultTable Execute(KnowledgeBase *knowledge_base) const noexcept final;
+};
+}  // namespace spa
+
+#endif  // SRC_SPA_SRC_QPS_CONDITIONS_CALLS_TRANS_CLAUSE_H_
