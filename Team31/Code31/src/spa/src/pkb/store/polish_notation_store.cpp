bc3f403026019a5312490bac4235528165dc7631
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-03-22 23:48:27 +0800

    PKB: polish notation store clean up
    Tie up the loose ends:
    - Avoid unnecessary copy
    - Use a proper function instead of switch case to create common nodes
    - General logic improvements

--- Team31/Code31/src/spa/src/pkb/store/polish_notation_store.cpp
+++ Team31/Code31/src/spa/src/pkb/store/polish_notation_store.cpp
@@ -1,127 +1,86 @@
 #include "polish_notation_store.h"
 
 #include <cassert>
+#include <utility>
 
 namespace spa {
 PolishNotationStore::PolishNotationStore(size_t stmt_count, std::vector<PN> pns,
-                                         const NameValueStore &name_value)
+                                         const NameValueStore& name_value)
         : stmt_to_index_(stmt_count + 1),
           index_to_stmt_(pns.size()),
           index_to_pn_(std::move(pns)),
           name_value_(name_value) {}
-
 void PolishNotationStore::Set(int stmt_no, int polish_index) {
     stmt_to_index_[stmt_no] = polish_index;
     index_to_stmt_[polish_index] = stmt_no;
 }
-
-const PN &PolishNotationStore::GetNotation(int polish_index) const {
+const PN& PolishNotationStore::GetNotation(int polish_index) const {
     return index_to_pn_[polish_index];
 }
-
 int PolishNotationStore::GetPolishStmt(int polish_index) const {
     return index_to_stmt_[polish_index];
 }
-
 int PolishNotationStore::GetPolishIndex(int stmt_no) const {
     return stmt_to_index_[stmt_no];
 }
-
-std::set<int> PolishNotationStore::CheckPattern(PN converted_pn,
-                                                bool partial_match,
-                                                std::vector<int> assign_stmt) {
+std::set<int> PolishNotationStore::CheckPattern(
+        const PN& converted_pn, bool partial_match,
+        const std::vector<int>& assign_stmt) {
     return partial_match ? CheckPatternP(converted_pn, assign_stmt)
                          : CheckPattern(converted_pn, assign_stmt);
 }
-
-std::set<int> PolishNotationStore::CheckPattern(PN converted_pn,
-                                                std::vector<int> assign_stmt) {
+std::set<int> PolishNotationStore::CheckPattern(
+        const PN& converted_pn, const std::vector<int>& assign_stmt) {
     std::set<int> full_match_stmt;
-
     for (const auto i : assign_stmt) {
-        const PN &pn = index_to_pn_[stmt_to_index_[i]];
+        const PN& pn = index_to_pn_[stmt_to_index_[i]];
         if (pn == converted_pn) {
             full_match_stmt.emplace(i);
         }
     }
     return full_match_stmt;
 }
-
-std::set<int> PolishNotationStore::CheckPatternP(PN converted_pn,
-                                                 std::vector<int> assign_stmt) {
+std::set<int> PolishNotationStore::CheckPatternP(
+        const PN& converted_pn, const std::vector<int>& assign_stmt) {
     std::set<int> partial_match_stmt;
-
     for (int i : assign_stmt) {
-        const PN &pn = index_to_pn_[stmt_to_index_[i]];
+        const PN& pn = index_to_pn_[stmt_to_index_[i]];
         if (pn.Contains(converted_pn)) {
             partial_match_stmt.emplace(i);
         }
     }
     return partial_match_stmt;
 }
-
 std::unique_ptr<PolishNotation> PolishNotationStore::ConvertFromQueryTokens(
-        const std::vector<QueryToken> &tokens) {
-    std::vector<PolishNotationNode> expr;
-    for (const auto &token : tokens) {
-        if (AddSingleToken(token, expr)) {
+        const std::vector<QueryToken>& tokens) {
+    expr_.clear();
+    for (const auto& token : tokens) {
+        if (!AddSingleToken(token)) {
             return {};
         }
     }
-    return std::make_unique<PolishNotation>(expr);
+    return std::make_unique<PolishNotation>(std::move(expr_));
 }
-
-PolishNotationNode PolishNotationStore::TokenToNotation(
-        QueryTokenType token_type) {
-    switch (token_type) {
-        case QueryTokenType::kOperatorPlus:
-            return PolishNotationNode(OperatorType::kPlus);
-        case QueryTokenType::kOperatorMinus:
-            return PolishNotationNode(OperatorType::kMinus);
-        case QueryTokenType::kOperatorTimes:
-            return PolishNotationNode(OperatorType::kTimes);
-        case QueryTokenType::kOperatorDivide:
-            return PolishNotationNode(OperatorType::kDivide);
-        case QueryTokenType::kOperatorModulo:
-            return PolishNotationNode(OperatorType::kModulo);
-        case QueryTokenType::kBracketL:
-            return PolishNotationNode(ExprNodeType::kBracketL);
-        case QueryTokenType::kBracketR:
-            return PolishNotationNode(ExprNodeType::kBracketR);
-        default:
-            assert(false);
-    }
+bool PolishNotationStore::AddVariable(const QueryToken& token) {
+    int index = name_value_.GetIndex(token.value, QueryEntityType::kVar);
+    if (index == 0) return false;
+    expr_.emplace_back(PolishNotationNode(ExprNodeType::kVariable, index));
+    return true;
 }
-
-bool PolishNotationStore::AddConstant(QueryToken token,
-                                      std::vector<PolishNotationNode> &expr) {
-    if (token.type == QueryTokenType::kWord) {
-        int var_index =
-                name_value_.GetIndex(token.value, QueryEntityType::kVar);
-        if (var_index == 0) {
-            return true;
-        }
-        expr.emplace_back(
-                PolishNotationNode(ExprNodeType::kVariable, var_index));
-        return false;
-    }
-    assert(token.type == QueryTokenType::kInteger);
-    int const_index =
-            name_value_.GetIndex(token.value, QueryEntityType::kConst);
-    if (const_index == 0) {
-        return true;
-    }
-    expr.emplace_back(PolishNotationNode(ExprNodeType::kConstant, const_index));
-    return false;
+bool PolishNotationStore::AddConstant(const QueryToken& token) {
+    int index = name_value_.GetIndex(token.value, QueryEntityType::kConst);
+    if (index == 0) return false;
+    expr_.emplace_back(PolishNotationNode(ExprNodeType::kConstant, index));
+    return true;
 }
-
-bool PolishNotationStore::AddSingleToken(
-        QueryToken token, std::vector<PolishNotationNode> &expr) {
-    bool contains_unseen = false;
+bool PolishNotationStore::AddSingleToken(const QueryToken& token) {
+    bool varconst_present = true;
     switch (token.type) {
         case QueryTokenType::kWord:
+            varconst_present = AddVariable(token);
+            break;
         case QueryTokenType::kInteger:
-            contains_unseen = AddConstant(token, expr);
+            varconst_present = AddConstant(token);
             break;
         case QueryTokenType::kOperatorPlus:
         case QueryTokenType::kOperatorMinus:
@@ -130,12 +89,19 @@ bool PolishNotationStore::AddSingleToken(
         case QueryTokenType::kOperatorModulo:
         case QueryTokenType::kBracketL:
         case QueryTokenType::kBracketR:
-            expr.emplace_back(TokenToNotation(token.type));
+            expr_.emplace_back(TokenTypeToPNNode(token.type));
             break;
         default:
             assert(false);
     }
-    return contains_unseen;
+    return varconst_present;
 }
-
+PolishNotationNode PolishNotationStore::TokenTypeToPNNode(
+        QueryTokenType token_type) noexcept {
+    assert(token_type >= QueryTokenType::kOperatorPlus &&
+           token_type <= QueryTokenType::kBracketR);
+    auto arg = kOperatorMap[static_cast<int>(token_type) -
+                            static_cast<int>(QueryTokenType::kOperatorPlus)];
+    return std::visit([](auto opr) { return PolishNotationNode(opr); }, arg);
+}
 }  // namespace spa
--- Team31/Code31/src/spa/src/pkb/store/polish_notation_store.h
+++ Team31/Code31/src/spa/src/pkb/store/polish_notation_store.h
@@ -1,10 +1,13 @@
 #ifndef SRC_SPA_SRC_PKB_STORE_POLISH_NOTATION_STORE_H_
 #define SRC_SPA_SRC_PKB_STORE_POLISH_NOTATION_STORE_H_
 
+#include <array>
 #include <set>
+#include <variant>
 #include <vector>
 
 #include "common/polish_notation.h"
+#include "common/polish_notation_node.h"
 #include "name_value_store.h"
 #include "qps/query_token.h"
 
@@ -13,32 +16,40 @@ using PN = spa::PolishNotation;
 
 class PolishNotationStore {
   public:
-    explicit PolishNotationStore(size_t stmt_count, std::vector<PN> pns,
-                                 const NameValueStore &name_value);
+    PolishNotationStore(size_t stmt_count, std::vector<PN> pns,
+                        const NameValueStore& name_value);
     void Set(int stmt_no, int polish_index);
-    [[nodiscard]] const PN &GetNotation(int polish_index) const;
+    [[nodiscard]] const PN& GetNotation(int polish_index) const;
     [[nodiscard]] int GetPolishStmt(int polish_index) const;
     [[nodiscard]] int GetPolishIndex(int stmt_no) const;
-    [[nodiscard]] std::set<int> CheckPattern(PN converted_pn,
-                                             bool partial_match,
-                                             std::vector<int> assign_stmt);
-    [[nodiscard]] std::set<int> CheckPattern(PN converted_pn,
-                                             std::vector<int> assign_stmt);
-    [[nodiscard]] std::set<int> CheckPatternP(PN converted_pn,
-                                              std::vector<int> assign_stmt);
+    [[nodiscard]] std::set<int> CheckPattern(
+            const PN& converted_pn, bool partial_match,
+            const std::vector<int>& assign_stmt);
+    [[nodiscard]] std::set<int> CheckPattern(
+            const PN& converted_pn, const std::vector<int>& assign_stmt);
+    [[nodiscard]] std::set<int> CheckPatternP(
+            const PN& converted_pn, const std::vector<int>& assign_stmt);
     [[nodiscard]] std::unique_ptr<PolishNotation> ConvertFromQueryTokens(
-            const std::vector<QueryToken> &tokens);
-    [[nodiscard]] PolishNotationNode TokenToNotation(QueryTokenType token_type);
-    [[nodiscard]] bool AddConstant(QueryToken token,
-                                   std::vector<PolishNotationNode> &expr);
-    [[nodiscard]] bool AddSingleToken(QueryToken token,
-                                      std::vector<PolishNotationNode> &expr);
+            const std::vector<QueryToken>& tokens);
 
   private:
+    [[nodiscard]] bool AddSingleToken(const QueryToken& token);
+    [[nodiscard]] bool AddConstant(const QueryToken& token);
+    using PNNodeArgType = std::variant<OperatorType, ExprNodeType>;
+    static constexpr std::array<PNNodeArgType, 7> kOperatorMap{
+            OperatorType::kPlus,    OperatorType::kMinus,
+            OperatorType::kTimes,   OperatorType::kDivide,
+            OperatorType::kModulo,  ExprNodeType::kBracketL,
+            ExprNodeType::kBracketR};
+    static PolishNotationNode TokenTypeToPNNode(
+            QueryTokenType token_type) noexcept;
+
     std::vector<int> stmt_to_index_;
     std::vector<int> index_to_stmt_;
     std::vector<PN> index_to_pn_;
-    const NameValueStore &name_value_;
+    const NameValueStore& name_value_;
+    std::vector<PolishNotationNode> expr_;
+    bool AddVariable(const QueryToken& token);
 };
 }  // namespace spa
 
