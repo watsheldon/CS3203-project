8b9379cd5a9bd3d5da2612affa42c8eb8c594d2e
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-03-29 02:57:50 +0800

    Add Affects clause

--- Team31/Code31/src/spa/src/qps/conditions/affects_clause.cpp
+++ Team31/Code31/src/spa/src/qps/conditions/affects_clause.cpp
@@ -1,6 +1,34 @@
 #include "affects_clause.h"
 
+#include <utility>
+
+#include "common/entity_type_enum.h"
+#include "common/index.h"
+#include "common/type_convert_helpers.h"
 #include "pkb/knowledge_base.h"
 #include "qps/evaluator/result_table.h"
+#include "qps/synonym.h"
 
-namespace spa {}  // namespace spa
+namespace spa {
+ResultTable AffectsClause::NumNum(KnowledgeBase *pkb, StmtNo first,
+                                  StmtNo second) const noexcept {
+    auto result = pkb->ExistAffects(Index<ArgPos::kFirst>(first),
+                                    Index<ArgPos::kSecond>(second));
+    return ResultTable(result);
+}
+ResultTable AffectsClause::NumSyn(KnowledgeBase *pkb, StmtNo first,
+                                  Synonym *second) const noexcept {
+    auto result = pkb->GetAffects(Index<ArgPos::kFirst>(first));
+    return {second, std::move(result)};
+}
+ResultTable AffectsClause::SynNum(KnowledgeBase *pkb, Synonym *first,
+                                  StmtNo second) const noexcept {
+    auto result = pkb->GetAffects(Index<ArgPos::kSecond>(second));
+    return {first, std::move(result)};
+}
+ResultTable AffectsClause::SynSyn(KnowledgeBase *pkb, Synonym *first,
+                                  Synonym *second) const noexcept {
+    auto [col_1, col_2] = pkb->GetAffectsPairs();
+    return {first, std::move(col_1), second, std::move(col_2)};
+}
+}  // namespace spa
