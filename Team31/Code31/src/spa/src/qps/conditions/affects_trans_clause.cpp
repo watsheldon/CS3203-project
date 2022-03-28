ad754373724f2fbe54de76528d42d86cfe6dbc6f
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-03-29 03:11:44 +0800

    Add AffectsTrans clause

--- Team31/Code31/src/spa/src/qps/conditions/affects_trans_clause.cpp
+++ Team31/Code31/src/spa/src/qps/conditions/affects_trans_clause.cpp
@@ -1,6 +1,34 @@
 #include "affects_trans_clause.h"
 
+#include <utility>
+
+#include "common/entity_type_enum.h"
+#include "common/index.h"
+#include "common/type_convert_helpers.h"
 #include "pkb/knowledge_base.h"
 #include "qps/evaluator/result_table.h"
+#include "qps/synonym.h"
 
-namespace spa {}  // namespace spa
+namespace spa {
+ResultTable AffectsTransClause::NumNum(KnowledgeBase *pkb, StmtNo first,
+                                       StmtNo second) const noexcept {
+    auto result = pkb->ExistAffectsT(Index<ArgPos::kFirst>(first),
+                                     Index<ArgPos::kSecond>(second));
+    return ResultTable(result);
+}
+ResultTable AffectsTransClause::NumSyn(KnowledgeBase *pkb, StmtNo first,
+                                       Synonym *second) const noexcept {
+    auto result = pkb->GetAffectsT(Index<ArgPos::kFirst>(first));
+    return {second, std::move(result)};
+}
+ResultTable AffectsTransClause::SynNum(KnowledgeBase *pkb, Synonym *first,
+                                       StmtNo second) const noexcept {
+    auto result = pkb->GetAffectsT(Index<ArgPos::kSecond>(second));
+    return {first, std::move(result)};
+}
+ResultTable AffectsTransClause::SynSyn(KnowledgeBase *pkb, Synonym *first,
+                                       Synonym *second) const noexcept {
+    auto [col_1, col_2] = pkb->GetAffectsTPairs();
+    return {first, std::move(col_1), second, std::move(col_2)};
+}
+}  // namespace spa
--- Team31/Code31/src/spa/src/qps/conditions/affects_trans_clause.h
+++ Team31/Code31/src/spa/src/qps/conditions/affects_trans_clause.h
@@ -1,14 +1,24 @@
 #ifndef SRC_SPA_SRC_QPS_CONDITIONS_AFFECTS_TRANS_CLAUSE_H_
 #define SRC_SPA_SRC_QPS_CONDITIONS_AFFECTS_TRANS_CLAUSE_H_
 
+#include "affects_base.h"
 #include "pkb/knowledge_base.h"
 #include "qps/evaluator/result_table.h"
-#include "stmt_stmt_base.h"
 
 namespace spa {
-class AffectsTransClause : public StmtStmtBase {
+class AffectsTransClause : public AffectsBase {
   public:
-    using StmtStmtBase::StmtStmtBase;
+    using AffectsBase::AffectsBase;
+
+  protected:
+    ResultTable NumNum(KnowledgeBase *pkb, StmtNo first,
+                       StmtNo second) const noexcept final;
+    ResultTable NumSyn(KnowledgeBase *pkb, StmtNo first,
+                       Synonym *second) const noexcept final;
+    ResultTable SynNum(KnowledgeBase *pkb, Synonym *first,
+                       StmtNo second) const noexcept final;
+    ResultTable SynSyn(KnowledgeBase *pkb, Synonym *first,
+                       Synonym *second) const noexcept final;
 };
 }  // namespace spa
 #endif  // SRC_SPA_SRC_QPS_CONDITIONS_AFFECTS_TRANS_CLAUSE_H_
