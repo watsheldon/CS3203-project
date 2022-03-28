0ce2a73074f4decd01b9c65230f72f4b6f797b45
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-03-28 23:04:09 +0800

    Add validator unit testcases for such-that cl

--- Team31/Code31/src/unit_testing/src/qps/PQL_validator_test.cpp
+++ Team31/Code31/src/unit_testing/src/qps/PQL_validator_test.cpp
@@ -7,31 +7,86 @@
 
 namespace spa {
 TEST_CASE("common/PQLValidator") {
+    QueryToken decl_assign = QueryToken(QueryTokenType::kDeclAssign);
+    QueryToken decl_proc = QueryToken(QueryTokenType::kDeclProcedure);
+    QueryToken decl_while = QueryToken(QueryTokenType::kDeclWhile);
+    QueryToken comma = QueryToken(QueryTokenType::kComma);
+    QueryToken semicolon = QueryToken(QueryTokenType::kSemicolon);
+    QueryToken decl_variable = QueryToken(QueryTokenType::kDeclVariable);
+    QueryToken select = QueryToken(QueryTokenType::kKeywordSelect);
+    QueryToken pattern = QueryToken(QueryTokenType::kKeywordPattern);
+    QueryToken dot = QueryToken(QueryTokenType::kDot);
+    QueryToken attr_proc = QueryToken(QueryTokenType::kAttrProc);
+    QueryToken such = QueryToken(QueryTokenType::kKeywordSuch);
+    QueryToken that = QueryToken(QueryTokenType::kKeywordThat);
+    QueryToken calls = QueryToken(QueryTokenType::kKeywordCalls);
+    QueryToken parent = QueryToken(QueryTokenType::kKeywordParent);
+    QueryToken next = QueryToken(QueryTokenType::kKeywordNext);
+    QueryToken bracket_l = QueryToken(QueryTokenType::kBracketL);
+    QueryToken bracket_r = QueryToken(QueryTokenType::kBracketR);
+    QueryToken quote = QueryToken(QueryTokenType::kQuote);
+    QueryToken wildcard = QueryToken(QueryTokenType::kUnderscore);
+    QueryToken times = QueryToken(QueryTokenType::kOperatorTimes);
+    QueryToken and_token = QueryToken(QueryTokenType::kKeywordAnd);
     SECTION("next (basic queries)") {
         std::string_view query_string1{
                 "procedure p, q; Select p.procName such that Calls (p, q)"};
         PQLValidator pql_validator1;
         auto tokens = pql_validator1.Validate(query_string1);
-        QueryToken decl_proc = QueryToken(QueryTokenType::kDeclProcedure);
         QueryToken word_token_p = QueryToken(QueryTokenType::kWord, "p");
-        QueryToken comma = QueryToken(QueryTokenType::kComma);
         QueryToken word_token_q = QueryToken(QueryTokenType::kWord, "q");
-        QueryToken semicolon = QueryToken(QueryTokenType::kSemicolon);
-        QueryToken decl_variable = QueryToken(QueryTokenType::kDeclVariable);
-        QueryToken select = QueryToken(QueryTokenType::kKeywordSelect);
-        QueryToken dot = QueryToken(QueryTokenType::kDot);
-        QueryToken attr_proc = QueryToken(QueryTokenType::kAttrProc);
-        QueryToken such = QueryToken(QueryTokenType::kKeywordSuch);
-        QueryToken that = QueryToken(QueryTokenType::kKeywordThat);
-        QueryToken calls = QueryToken(QueryTokenType::kKeywordCalls);
-        QueryToken bracket_l = QueryToken(QueryTokenType::kBracketL);
-        QueryToken bracket_r = QueryToken(QueryTokenType::kBracketR);
         std::vector<QueryToken> expected{
                 decl_proc,    word_token_p, comma,     word_token_q, semicolon,
                 select,       word_token_p, dot,       attr_proc,    such,
                 that,         calls,        bracket_l, word_token_p, comma,
                 word_token_q, bracket_r};
         REQUIRE(expected == tokens);
+    }
+    SECTION("next (queries of full suchthat-cl)") {
+        std::string_view query_string2{
+                "assign a; while w;Select a pattern a (\"x\", _) such that "
+                "Parent* (w, a) and Next* (1, a)"};
+        PQLValidator pql_validator2;
+        auto tokens2 = pql_validator2.Validate(query_string2);
+        QueryToken word_token_a = QueryToken(QueryTokenType::kWord, "a");
+        QueryToken word_token_w = QueryToken(QueryTokenType::kWord, "w");
+        QueryToken word_token_x = QueryToken(QueryTokenType::kWord, "x");
+        QueryToken integer_token_1 = QueryToken(QueryTokenType::kInteger, "1");
+        std::vector<QueryToken> expected{decl_assign,
+                                         word_token_a,
+                                         semicolon,
+                                         decl_while,
+                                         word_token_w,
+                                         semicolon,
+                                         select,
+                                         word_token_a,
+                                         pattern,
+                                         word_token_a,
+                                         bracket_l,
+                                         quote,
+                                         word_token_x,
+                                         quote,
+                                         comma,
+                                         wildcard,
+                                         bracket_r,
+                                         such,
+                                         that,
+                                         parent,
+                                         times,
+                                         bracket_l,
+                                         word_token_w,
+                                         comma,
+                                         word_token_a,
+                                         bracket_r,
+                                         and_token,
+                                         next,
+                                         times,
+                                         bracket_l,
+                                         integer_token_1,
+                                         comma,
+                                         word_token_a,
+                                         bracket_r};
+        REQUIRE(expected == tokens2);
     }
 }
 }  // namespace spa
