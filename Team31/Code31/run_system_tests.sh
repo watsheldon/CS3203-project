d8f407e63d1215da270c40c17a9c7a4f5a860c70
Kou Yong Kang <skykinetic@stevenkou.xyz>
2022-04-02 01:46:00 +0800

    Add braces to all quoted variables

--- Team31/Code31/run_system_tests.sh
+++ Team31/Code31/run_system_tests.sh
@@ -30,22 +30,22 @@ else
 fi
 echo "System test directory: ${test_dir}"
 
-readarray -d '' simple_sources < <(find $test_dir -type f -name "*$SOURCE_SUFFIX" -print0)
+readarray -d '' simple_sources < <(find $test_dir -type f -name "*${SOURCE_SUFFIX}" -print0)
 for source in "${simple_sources[@]}"; do
-  queries="${source/$SOURCE_SUFFIX/$QUERY_SUFFIX}"
-  if [ ! -f "$queries" ]; then
+  queries="${source/${SOURCE_SUFFIX}/${QUERY_SUFFIX}}"
+  if [ ! -f "${queries}" ]; then
     echo "Matching file for ${source} does not exist, skipping..."
     continue
   fi
-  name=$(basename "$source" $SOURCE_SUFFIX)
+  name=$(basename "${source}" ${SOURCE_SUFFIX})
   echo -n "Running ${name}... "
-  output="$OUTPUT_DIR/$name$XML_EXT"
-  cmd="$autotester $source $queries $output"
+  output="${OUTPUT_DIR}/${name}${XML_EXT}"
+  cmd="${autotester} ${source} ${queries} ${output}"
   if $cmd &>$dev_null; then
-    total_queries=$(grep -o "</query>" "$output" | wc -l)
-    failed_queries=$(grep -o "</failed>" "$output" | wc -l)
-    if [ "$failed_queries" -eq 0 ]; then color=$GREEN; else color=$RED; fi
-    echo -e "${color}${BOLD}$((total_queries - failed_queries))/$total_queries passed${NO_STYLE}"
+    total_queries=$(grep -o "</query>" "${output}" | wc -l)
+    failed_queries=$(grep -o "</failed>" "${output}" | wc -l)
+    if [ "${failed_queries}" -eq 0 ]; then color=$GREEN; else color=$RED; fi
+    echo -e "${color}${BOLD}$((total_queries - failed_queries))/${total_queries} passed${NO_STYLE}"
   else
     echo -e "${RED}${BOLD}error encountered!${NO_STYLE}"
   fi
